898e3adda8c3693a867eb01a400da7306a2c571f
ruhma-nazir <ruhma.nazir@devsinc.com>
2025-08-11 18:38:32 +0500

    forms addition

--- product/forms.py
+++ product/forms.py
@@ -1,2 +1,9 @@
-from django import forms 
+from django import forms
 from product.models import *
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=50, required=True)
+    description = forms.CharField(max_length=350, required=True, widget=forms.Textarea)
+    price = forms.FloatField(min_value=0, max_value=1000000)
+    in_stock = forms.BooleanField(required=True)
